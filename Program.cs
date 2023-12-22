99b629631bf57cce32d9ac2aee67fd0d996786d2
CamilaCPG <camila.cpguedes@gmail.com>
2023-12-22 13:18:09 -0300

    Concluídogit status

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("No Smartphone NOKIA:");
+Smartphone nokia = new Nokia(numero: "87381273", modelo: "132", imei: "1238497", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("No Smartphone IPHONE:");
+Smartphone iphone = new Iphone(numero: "90531742", modelo: "728", imei: "0192832", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Candy Crush");
